050d064202fd706dc88169c121cbc710d78041a0
josewerlich <werlich@outlook.com>
2025-03-20 19:25:56 -0400

    Version 1: User data entry and basic structure

--- Jogo da Adivinhacao/Program.cs
+++ Jogo da Adivinhacao/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        // Version 1: User data entry and basic structure
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Guess the Number");
+            Console.WriteLine("---------------------------------");
+
+            // guessing logic
+
+;          
+
+            Console.Write("Type a number from 1 to 20: ");
+            int guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You typed the number: " + guessedNumber);
+            
+
+            Console.ReadLine();
         }
     }
 }
