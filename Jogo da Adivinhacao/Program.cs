1a95d0996e1ef19a068d60550b640eea0bcf13e9
josewerlich <werlich@outlook.com>
2025-03-20 19:34:42 -0400

    Version 3: Validate if player guessed the correct number

--- Jogo da Adivinhacao/Program.cs
+++ Jogo da Adivinhacao/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        // Version 2: Generate a random number
+        // Version 3: Validate if player guessed the correct number
         static void Main(string[] args)
         {
             Console.WriteLine("---------------------------------");
@@ -18,8 +18,29 @@
             Console.Write("Type a number from 1 to 20: ");
             int guessedNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("You typed the number: " + guessedNumber);
-            Console.WriteLine("The secret number is: " + secretNumber);
+            if(guessedNumber == secretNumber)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Congratulations, you guessed the secret number");
+                Console.WriteLine("---------------------------------");
+
+            }
+            else if (guessedNumber > secretNumber)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("The guessed number is higher than the secret number");
+                Console.WriteLine("---------------------------------");
+
+            }
+
+            else
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("The guessed number is lower than the secret number");
+                Console.WriteLine("---------------------------------");
+            }
+                Console.WriteLine("You typed the number: " + guessedNumber);
+                Console.WriteLine("The secret number is: " + secretNumber);
 
             Console.ReadLine();
         }
